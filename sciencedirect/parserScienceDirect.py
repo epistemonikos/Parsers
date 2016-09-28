33de6e9e882062b8f8de56840b939aded0da7418
Fabian Mosso Chavez <fmosso@klabs.cl>
2016-09-28 17:00:42 -0300

    updated parser for paper inside pyment wall

--- sciencedirect/parserScienceDirect.py
+++ sciencedirect/parserScienceDirect.py
@@ -5,42 +5,51 @@ import re
 
 
 def parser():
-    soup = BeautifulSoup(open('Assessment of cardiovascular risk by use of multiple-risk-factor assessment equations_ A statement for healthcare professionals from the American Heart Association and the American College of Cardiology11This statement was approved by the American He.html'), 'html.parser')
+    soup = BeautifulSoup(open('test2.html'), 'html.parser')
     resp = {}
 
     # get the authors
-    authors = soup.body.find('ul', 'authorGroup noCollab svAuthor')
-    resp['authors'] = []
-    for author in authors.find_all('li'):
-        c= {}
-        c = author.a['data-fn']+" "+author.a['data-ln']
-        resp['authors'].append(c)
-    references = soup.body.find_all('div', 'refContent')
-    citations = []
-
+    def addAuthors(author):
+         c= {}
+         c = author
+         resp['authors'].append(c)
+    if soup.body.find('ul', 'authorGroup noCollab svAuthor'):
+        resp['authors'] = []
+        for author in soup.body.find('ul', 'authorGroup noCollab svAuthor').find_all('li'):
+         addAuthors(author.a['data-fn']+" "+author.a['data-ln'])
+    elif  soup.body.find('div', 'article-author-list'):
+        resp['authors'] = []
+        for author in soup.body.find('div', 'article-author-list').find_all('span','author-name'):
+            addAuthors(author.a.get_text()[1:])
     # get the abstract
-    resp['abstract'] = {}
     abstract = soup.body.find('div','abstract svAbstract ')
     c={}
     if abstract:
         for titles, texts in zip(abstract.find_all('h4'), abstract.find_all('p')):
             c[titles.get_text()] = texts.get_text()
         resp['abstract'] = c
-    else:
-
     # get the doi
-    resp['doi'] = soup.body.find('dd','doi').get_text()
+    if soup.body.find('dd','doi'):
+        resp['doi'] = soup.body.find('dd','doi').get_text()
+    elif soup.body.find('p','article-doi'):
+        resp['doi'] = soup.body.find('p','article-doi').a['href']
     #get journal
-    resp['journal'] = soup.body.find('div','publicationHead').find('span').get_text()
+    if soup.body.find('div','publicationHead'):
+        resp['journal'] = soup.body.find('div','publicationHead').find('span').get_text()
+    elif soup.body.find('p','journal-title'):
+        resp['journal'] = soup.body.find('p','journal-title').a.get_text()
     #get keyword
-    k =[]
-    for keywords in soup.body.find('ul','keyword').find_all('li'):
-        k.append(keywords.find('span').get_text())
-    resp['keywords'] = k
+    if soup.body.find('ul','keyword'):
+      k =[]
+      for keywords in soup.body.find('ul','keyword').find_all('li'):
+          k.append(keywords.find('span').get_text())
+      resp['keywords'] = k
     #get name
-    resp['name'] = soup.body.find('h1','svTitle').get_text()
+    if soup.body.find('h1','svTitle'):
+        resp['name'] = soup.body.find('h1','svTitle').get_text()
+    elif soup.body.find('h1','article-title'):
+        resp['name'] = soup.body.find('h1','article-title').get_text()
     #get reference
-    resp['references'] = []
     r =[]
     for groupofreferences in soup.body.find_all('ol','references'):
         for references in groupofreferences.find_all('ul', 'reference'):
@@ -67,23 +76,31 @@ def parser():
             if references.find('li','source') and references.find('li','source').p:
                 ref['source'] = references.find('li','source').p.contents[0]
             r.append(ref)
-    resp['references'] = r
+    if r:
+        resp['references'] = r
     #get the volume
-    volume = soup.body.find('p','volIssue')
-    resp['volume'] = volume.a.get_text()
-    #get date
-    resp['date'] = re.search(r', (.*),', volume.contents[1]).group(1)
+
+    if soup.body.find('p','volIssue'):
+        volume = soup.body.find('p','volIssue')
+    elif soup.body.find('p','journal-volume'):
+        volume = soup.body.find('p','journal-volume')
+    if volume:
+        resp['volume'] = volume.a.get_text()
+        #get date
+        resp['date'] = re.search(r', (.*),', volume.contents[1]).group(1)
+
+
     #get cited articules
     listcitedarticules = soup.body.find('ol',{'id':'citedByList'})
-    ca = []
     if listcitedarticules:
+        ca = []
         for articules in listcitedarticules.find_all('ol', 'articles'):
             citedarticules = {}
             citedarticules['name'] = articules.find('li', 'artTitle').a['title']
             citedarticules['ref'] = articules.find('li', 'artTitle').a['href']
             citedarticules['source'] = articules.find('li', 'srcTitle').get_text()
             ca.append(citedarticules)
-    resp['cited articules'] = ca
+        resp['cited articules'] = ca
     json_data = json.dumps(resp)
     print (json_data)
 parser()
